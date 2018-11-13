128c565443fbf0689e05b93029495d17a893691f
noriaki-yamada <noriaki.yamada@hakuhodo.co.jp>
2018-11-13 19:45:30 +0900

    :tada: init

---
+++ nuxt.config.js
@@ -0,0 +1,70 @@
+const TerserPlugin = require('terser-webpack-plugin')
+
+module.exports = {
+  /*
+  ** Headers of the page
+  */
+  head: {
+    htmlAttrs: {
+      lang: 'ja'
+    },
+    title: 'nuxt as Static Site Generator',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+  transition: {
+    name: 'hoge',
+    mode: 'out-in'
+  },
+  css: [
+    '@/assets/style.css'
+  ],
+  router: {
+    base: '/'
+  },
+  /*
+  ** Customize the progress bar color
+  */
+  loading: { color: '#3B8070' },
+  /*
+  ** Build configuration
+  */
+  build: {
+    /*
+    ** Run ESLint on save
+    */
+    extend (config, { isDev, isClient }) {
+      if (isDev && isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/
+        })
+      }
+
+      if (isClient) {
+        if (!config.optimization.minimizer) {
+          config.optimization.minimizer = []
+        }
+
+        config.optimization.minimizer.push(
+          new TerserPlugin({
+            terserOptions: {
+              compress: {
+                drop_console: process.env.NODE_ENV === 'production'
+              }
+            }
+          })
+        )
+      }
+    }
+  }
+}
+
